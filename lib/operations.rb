1350d4b9b4038b345d960341af02073e8c3e2a13
Andrew Kloecker <andrew.kloecker@MacBook-Pro.local>
2020-04-28 17:17:04 -0400

    complete lab

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,16 @@
 def unsafe?(speed)
-
+    # !(speed < 60 and speed > 40)
+    if speed > 60 or speed < 40
+        true
+    else
+        false
+    end 
 end
 
 
 
 def not_safe?(speed)
-	
+	speed > 60 or speed < 40 ? true : false
 end
 	
 
