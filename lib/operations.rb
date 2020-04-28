bd90aa0a94424105aeee65591b31460d4906ca58
Pat McClernan <patmccler@gmail.com>
2020-04-28 16:55:58 -0400

    build out unsafe and not safe methods

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,19 @@
 def unsafe?(speed)
+    #speed > 60 || speed < 40
 
+    if speed < 40 
+        true
+    elsif speed >= 40 && speed <= 60
+        false
+    else #over 60
+        true
+    end
 end
 
 
 
 def not_safe?(speed)
-	
+	(speed > 40 && speed < 60) ? false : true
 end
 	
 
