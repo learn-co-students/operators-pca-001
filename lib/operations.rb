11b928dca0e988cfaca15176a9f88f99099fd6d3
Madison McCardle <madison.mccardle@powerhrg.com>
2020-04-28 23:10:31 -0400

    if else

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,16 @@
 def unsafe?(speed)
-
+    speed<40 || speed>60 
 end
 
-
+#def unsafe?(speed)
+    #if speed<40 || speed>60 
+        #true
+    #else
+        #false
+#end
 
 def not_safe?(speed)
-	
+	speed<40 || speed>60 ? true : false 
 end
 	
 
